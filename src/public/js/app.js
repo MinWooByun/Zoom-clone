9d613db79df0a3169e01c33860172521fbfd2ab9
MinWooByun <yja05092@gmail.com>
2022-07-05 21:29:28 +0900

    WebSocket Events and Messages
    WebSocket의 이벤트 처리와 frontend와 backend 사이의 메시지 교환

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server 👍");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log(`New message : ${message.data}`);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconected from Server 👎");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,6 +9,25 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
-console.log("hello");
+const handleListen = () => console.log("Listening on http://localhost:3000");
+// 아래의 두 코드로 http 서버와 WebSocket 서버를 동시에 돌릴 수 있다. 필수로 2개를 돌릴 필요는 없다.
+// 2개다 만드는 이유는 같은 3000번 포트를 사용하기 위해서이다.
+// http 서버
+const server = http.createServer(app);
+// WebSocket 서버
+const wss = new WebSocket.Server({ server });
 
-app.listen(3000);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser 👍");
+  socket.on("close", () => {
+    console.log("Disconected from Browser 👎");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+  socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
+
+// app.listen(3000, handleListen);
